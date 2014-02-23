645237b89b67062ae3e17c4a2838bfdb9d2be367
Justin Steffen <JustinDSN@gmail.com>
2014-02-22 19:30:40 -0800

    Finishing quiz project.  App LaunchImage is missing from Big Nerd Ranch resources download.

---
+++ BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Justin Steffen on 2/20/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ BNRQuizViewController.m
@@ -0,0 +1,62 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Justin Steffen on 2/20/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+- (instancetype) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    // Return the address of the new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender {
+    self.currentQuestionIndex++;
+    
+    if (self.currentQuestionIndex == [self.questions count]) {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(id)sender {
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
+}
+
+@end
